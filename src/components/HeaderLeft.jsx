12b3e3476cbb5de537acfd17f758a00abd280d09
KaykePeresD <kaykeperes2502@gmail.com>
2025-11-29 13:34:17 -0300

    Update

--- src/components/HeaderLeft.jsx
+++ src/components/HeaderLeft.jsx
@@ -1,134 +1,182 @@
-import React from 'react';
-import image from '/public/img3-perfil.jpg';
-import { Sun, Moon, MapPinned, Mail, MailPlus, Github, Linkedin } from 'lucide-react';
+import React from "react";
+import image from "/public/profile2.png";
+import {
+  Sun,
+  Moon,
+  MapPinned,
+  Mail,
+  MailPlus,
+  Github,
+  Linkedin,
+} from "lucide-react";
+import aws from "/public/awscp.png";
 
-const HeaderLeft = ({darkMode, toggleDarkMode}) => {
+const HeaderLeft = ({ darkMode, toggleDarkMode }) => {
   return (
-    <div className='min-h-screen flex-1 relative p-10 transition-all  duration-500 dark:bg-black dark:text-white
-    sm:border-r dark:border-gray-700'>
-        <div className='flex flex-col gap-5'>
+    <div
+      className="min-h-screen flex-1 relative p-10 transition-all  duration-500 dark:bg-black dark:text-white
+    sm:border-r dark:border-gray-700"
+    >
+      <div className="flex flex-col gap-5">
+        <div className="flex flex-col items-center justify-center gap-5">
+          <img
+            src={image}
+            alt="profile"
+            className="w-36 h-36 rounded-full object-cover"
+          />
 
-           <div className='flex flex-col items-center justify-center gap-5'>
-
-             <img 
-  src={image} 
-  alt="profile" 
-  className="w-32 h-32 rounded-full object-cover"
-/>
-
-             <div className='text-center space-y-1'>
-                <h1 className='text-4xl font-light'>
-                    Kayke <span className='font-semibold'>Peres</span>
-                </h1>
-                <h3 className='text-xl font-light'>
-                    software developer
-                </h3>
-             </div>
-             <button className='absolute right-10 top-10 ' onClick={toggleDarkMode}>
-                {
-                    darkMode ? (
-                        <Sun className='text-2xl '/>
-                    ) : (
-                        <Moon className='text-2xl'/>
-                    )
-                }
-             </button>
+          <div className="text-center space-y-1">
+            <h1 className="text-4xl font-light">
+              Kayke <span>Peres</span>
+            </h1>
+            <h3 className="text-xl font-light">DevOps Engineer</h3>
           </div>
-
-
-          <div className="flex flex-col items-start justify-start gap-5 border-b p-5 dark:border-gray-700">
-            <ul className='space-y-3 '>
-                <li className='flex items-center gap-2'>
-                  <MapPinned className='text-xl'/>
-                  <span>São Paulo, Suzano</span>
-                </li>
-                <li className='flex items-center gap-2 '>
-                 <Mail className='text-xl'/>
-                  <span>kaykeperes2910@gmail.com  <span className='text-gray-500'>ou,</span></span>
-                </li>
-                 <li className='flex items-center gap-2'>
-                 <MailPlus className='text-xl'/>
-                  <span>kaykeperes2502@gmail.com</span>
-                </li>
-            </ul>
-          </div>
-
-            <div className="flex flex-col items-start justify-start gap-5 border-b p-5 dark:border-b-gray-700">
-                <h1 className='text-base font-semibold md:text-2xl'>REDES SOCIAIS</h1>
-
-                <ul className='space-y-3'>
-                      <li className='flex items-center gap-2'>
-                  <Github className='text-xl'/>
-                  <span>@Github/KaykePeresD</span>
-                </li>
-                 <li className='flex items-center gap-2'>
-                 <Linkedin className='text-xl'/>
-                  <span>@Linkedin/Kayke-Peres</span>
-                </li>
-                </ul>
-            </div>
-
-            <div className="flex flex-col items-start justify-start gap-5 border-b p-5 dark:border-b-gray-700 ">
-                <h1 className='text-base font-semibold md:text-2xl'>FORMAÇÃO ACADÊMICA</h1>
-                <ul>
-                    <li className='relative flex flex-col gap-1 border-l p-6 before:absolute before:left-[-6px]
-                    before:top-1/2 before:h-3 before:w-3 before:translate-y-1/2 before:rounded-full before:bg-black
-                    dark:before:bg-white before:transform'>
-                        <span className='font-semibold md:text-lg'>Analíse Desenvolvimento de Sistemas</span>
-                        <span className='font-light'>Universidade Anhanguera</span>
-                        <span className='text-sm text-gray-400'>Em andamento</span>
-                        <span className='text-sm text-gray-400'>2025 - 2027</span>
-                    </li>
-                </ul>
-            </div>
-
-            <div className="flex flex-col items-start justify-start gap-5 border-b p-5 dark:border-b-gray-700 ">
-               <h1 className='text-base font-semibold md:text-2xl'>
-                STACK ATUAL
-               </h1>
-               <div className='flex flex-wrap gap-3'>
-
-                <div className="flex flex-wrap gap-3">
-                    <span className='rounded-xl bg-black text-white p-3 text-center dark:text-black 
-                    dark:bg-white'>HTML</span>
-                    <span className='rounded-xl bg-black text-white p-3 text-center dark:text-black 
-                    dark:bg-white'>CSS</span>
-                    <span className='rounded-xl bg-black text-white p-3 text-center dark:text-black 
-                    dark:bg-white'>JavaScript</span>
-                    <span className='rounded-xl bg-black text-white p-3 text-center dark:text-black 
-                    dark:bg-white'>ReactJS</span>
-                    <span className='rounded-xl bg-black text-white p-3 text-center dark:text-black 
-                    dark:bg-white'>TailwindCSS</span>
-                </div>
-               </div>
-            </div>
-            <div className="flex flex-col items-start justify-start gap-5 border-b p-5 dark:border-b-gray-700 ">
-               <h1 className='text-base font-semibold md:text-2xl'>
-                STACK EM EVOLUÇÃO
-               </h1>
-               <div className='flex flex-wrap gap-3'>
-
-                <div className="flex flex-wrap gap-3">
-                    <span className='rounded-xl bg-black text-white p-3 text-center dark:text-black 
-                    dark:bg-white'>NodeJs</span>
-                    <span className='rounded-xl bg-black text-white p-3 text-center dark:text-black 
-                    dark:bg-white'>DevOps</span>
-                    <span className='rounded-xl bg-black text-white p-3 text-center dark:text-black 
-                    dark:bg-white'>Python</span>
-                </div>
-               </div>
-            </div>
-
-            <div className="flex flex-col items-start justify-start gap-5 border-b p-5 dark:border-b-gray-700">
-              <h1 className='text-base font-semibold md:text-2xl'>IDIOMAS</h1>
-            <ul className='flex list-disc flex-wrap gap-12 md:text-lg '>
-            <li>Português - <span className='text-gray-500'>Idioma Nativo</span></li>
-            <li>Inglês - <span className='text-gray-500'>Compreendo textos inteiros. Mas com dificuldades em discussões.</span></li>      
-            </ul>    
-             </div>
+          <button
+            className="absolute right-10 top-10 "
+            onClick={toggleDarkMode}
+          >
+            {darkMode ? (
+              <Sun className="text-2xl " />
+            ) : (
+              <Moon className="text-2xl" />
+            )}
+          </button>
         </div>
-     </div>
-  )
-}
 
-export default HeaderLeft
+        <div className="flex flex-col items-start justify-start gap-5 border-b p-5 dark:border-gray-700">
+          <ul className="space-y-3 ">
+            <li className="flex items-center gap-2">
+              <MapPinned className="text-xl" />
+              <span>São Paulo, Suzano</span>
+            </li>
+            <li className="flex items-center gap-2 ">
+              <Mail className="text-xl" />
+              <span>
+                kaykeperes2910@gmail.com{" "}
+                <span className="text-gray-500">ou,</span>
+              </span>
+            </li>
+            <li className="flex items-center gap-2">
+              <MailPlus className="text-xl" />
+              <span>kaykeperes2502@gmail.com</span>
+            </li>
+          </ul>
+        </div>
+
+        <div className="flex flex-col items-start justify-start gap-5 border-b p-5 dark:border-b-gray-700">
+          <h1 className="text-base font-semibold md:text-2xl">REDES SOCIAIS</h1>
+
+          <ul className="space-y-3">
+            <li className="flex items-center gap-2">
+              <Github className="text-xl" />
+              <span>@Github/KaykePeresD</span>
+            </li>
+            <li className="flex items-center gap-2">
+              <Linkedin className="text-xl" />
+              <span>@Linkedin/Kayke-Peres</span>
+            </li>
+          </ul>
+        </div>
+
+        <div className="flex flex-col items-start justify-start gap-5 border-b p-5 dark:border-b-gray-700 ">
+          <h1 className="text-base font-semibold md:text-2xl">
+            FORMAÇÃO ACADÊMICA
+          </h1>
+          <ul>
+            <li
+              className="relative flex flex-col gap-1 border-l p-6 before:absolute before:left-[-6px]
+                    before:top-1/2 before:h-3 before:w-3 before:translate-y-1/2 before:rounded-full before:bg-black
+                    dark:before:bg-white before:transform"
+            >
+              <span className="font-semibold md:text-lg">
+                Analíse Desenvolvimento de Sistemas
+              </span>
+              <span className="font-light">Universidade Anhanguera</span>
+              <span className="text-sm text-gray-400">Em andamento</span>
+              <span className="text-sm text-gray-400">2025 - 2027</span>
+            </li>
+          </ul>
+        </div>
+
+        <div className="flex flex-col items-start justify-start gap-5 border-b p-5 dark:border-b-gray-700 ">
+          <h1 className="text-base font-semibold md:text-2xl">STACK ATUAL</h1>
+          <div className="flex flex-wrap gap-3">
+            <div className="flex flex-wrap gap-3">
+              <span
+                className="rounded-xl bg-black text-white p-3 text-center dark:text-black 
+                    dark:bg-white"
+              >
+                HTML
+              </span>
+              <span
+                className="rounded-xl bg-black text-white p-3 text-center dark:text-black 
+                    dark:bg-white"
+              >
+                CSS
+              </span>
+              <span
+                className="rounded-xl bg-black text-white p-3 text-center dark:text-black 
+                    dark:bg-white"
+              >
+                JavaScript
+              </span>
+              <span
+                className="rounded-xl bg-black text-white p-3 text-center dark:text-black 
+                    dark:bg-white"
+              >
+                ReactJS
+              </span>
+              <span
+                className="rounded-xl bg-black text-white p-3 text-center dark:text-black 
+                    dark:bg-white"
+              >
+                TailwindCSS
+              </span>
+              <span
+                className="rounded-xl bg-black text-white p-3 text-center dark:text-black 
+                    dark:bg-white"
+              >
+                Terraform
+              </span>
+              <span
+                className="rounded-xl bg-black text-white p-3 text-center dark:text-black 
+                    dark:bg-white"
+              >
+                AWS
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col items-start justify-start gap-5 border-b p-5 dark:border-b-gray-700 ">
+          <h1 className="text-base font-semibold md:text-2xl">
+            CERTIFICAÇÕES AWS
+          </h1>
+          <div className="flex flex-wrap gap-3">
+            <div className="flex flex-wrap gap-3">
+              <span className="w-36 h-36">
+                <img src={aws} alt="" />
+              </span>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex flex-col items-start justify-start gap-5  p-5 dark:border-b-gray-700">
+          <h1 className="text-base font-semibold md:text-2xl">IDIOMAS</h1>
+          <ul className="flex list-disc flex-wrap gap-12 md:text-lg ">
+            <li>
+              Português - <span className="text-gray-500">Idioma Nativo</span>
+            </li>
+            <li>
+              Inglês -{" "}
+              <span className="text-gray-500">
+                Compreendo textos inteiros. Mas com dificuldades em discussões.
+              </span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeaderLeft;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,42 +10,65 @@ const Home = () => {
         <div className="space-y-5 ">
           <h1 className="text-2xl font-bold md:text-4xl  ">Sobre</h1>
           <p className="text-sm text-gray-600 dark:text-gray-400 md:text-base">
-            Sou estudante de Análise e Desenvolvimento de Sistemas (2º
-            semestre), com foco em desenvolvimento web usando React e Tailwind
-            CSS. Tenho experiência como Jovem Aprendiz na área administrativa,
-            onde desenvolvi proatividade e trabalho em equipe. Atualmente,
-            também me aprofundo em DevOps e Python, buscando ampliar minha visão
-            técnica. Procuro oportunidades como estagiário ou desenvolvedor
-            júnior para aplicar meus conhecimentos em projetos reais e continuar
-            evoluindo na área.
+            Atualmente estou cursando Análise e Desenvolvimento de Sistemas e
+            trabalho na área de TI na CloudDog, integrando o time de Projetos.
+            No meu dia a dia, atuo com serviços da AWS e Infraestrutura como
+            Código utilizando Terraform, contribuindo para a construção e
+            manutenção de soluções em nuvem de forma automatizada e escalável.
+            Possuo a certificação AWS Cloud Practitioner e estou me preparando
+            para a certificação Associate, aprofundando meus conhecimentos em
+            arquitetura e serviços avançados da AWS. Sou apaixonado por aprender
+            coisas novas, estudar constantemente e evoluir continuamente como
+            profissional de tecnologia.
           </p>
         </div>
 
-        <div className="space-y-10 ">
+        <div className="space-y-10">
           <h1 className="text-2xl font-bold md:text-4xl">Experiência</h1>
 
-          <div className="flex flex-col lg:flex-row lg:gap-20">
-            <ul>
-              <li
-                className="relative flex flex-col gap-1 border-l p-6 before:absolute before:left-[-6px]
-                    before:top-1/2 before:h-3 before:w-3 before:translate-y-1/2 before:rounded-full before:bg-black
-                    dark:before:bg-white before:transform"
-              >
-                <span className="font-semibold md:text-lg">
-                  Jovem Aprendiz - Administração
-                </span>
-                <span className="font-light">CRAGEA</span>
-                <span className="text-sm text-gray-400">2024 - 2025</span>
-                <span className="text-sm text-gray-800 dark:text-gray-200">
-                  Auxiliei em tarefas administrativas, adquirindo habilidades de
-                  organização e gestão. Aprendi sobre processos empresariais e
-                  boas práticas administrativas. Desenvolvi competências de
-                  comunicação e trabalho em equipe. Bom uso em aplicativos como:
-                  Excel, Word, E-mail e TOTVS Tokyo empresarial
-                </span>
-              </li>
-            </ul>
-          </div>
+          <ul className="border-l">
+            {/* DevOps */}
+            <li
+              className="relative flex flex-col gap-1 p-6 
+        before:absolute before:left-[-6px] before:top-1/2 
+        before:h-3 before:w-3 before:translate-y-1/2 before:rounded-full 
+        before:bg-black dark:before:bg-white"
+            >
+              <span className="font-semibold md:text-lg">DevOps Engineer</span>
+              <span className="font-light">CloudDog</span>
+              <span className="text-sm text-gray-400">2025 - Atual</span>
+              <span className="text-sm text-gray-800 dark:text-gray-200">
+                Atuo no time de Projetos, desenvolvendo e mantendo
+                infraestruturas em nuvem utilizando serviços AWS e
+                Infraestrutura como Código com Terraform, responsável por criar,
+                versionar e automatizar recursos cloud, garantindo ambientes
+                escaláveis, seguros e padronizados. Também participo da
+                implementação de pipelines, otimização de processos,
+                contribuindo para entregas mais rápidas, confiáveis e alinhadas
+                às boas práticas de DevOps.
+              </span>
+            </li>
+
+            <li
+              className="relative flex flex-col gap-1 p-6 
+        before:absolute before:left-[-6px] before:top-1/2 
+        before:h-3 before:w-3 before:translate-y-1/2 before:rounded-full 
+        before:bg-black dark:before:bg-white"
+            >
+              <span className="font-semibold md:text-lg">
+                Jovem Aprendiz - Administração
+              </span>
+              <span className="font-light">CRAGEA</span>
+              <span className="text-sm text-gray-400">2024 - 2025</span>
+              <span className="text-sm text-gray-800 dark:text-gray-200">
+                Auxiliei em tarefas administrativas, adquirindo habilidades de
+                organização e gestão. Aprendi sobre processos empresariais e
+                boas práticas administrativas. Desenvolvi competências de
+                comunicação e trabalho em equipe. Bom uso em aplicativos como
+                Excel, Word, E-mail e TOTVS Tokyo empresarial.
+              </span>
+            </li>
+          </ul>
         </div>
 
         <div className="max-w-3xl space-y-8 ">
@@ -53,6 +76,23 @@ const Home = () => {
             Licenças e Certificados
           </h1>
           <div className="space-y-10">
+            <div className="text-baseline space-y-3">
+              <h3 className="text-sm font-semibold md:text-lg">
+                AWS Cloud Practitioner (2025)
+              </h3>
+              <p className="text-sm text-gray-600 dark:text-gray-400 md:text-base">
+                A certificação AWS Cloud Practitioner valida meu conhecimento
+                fundamental sobre computação em nuvem e sobre os principais
+                serviços, conceitos e práticas da AWS. Com ela, demonstro
+                compreensão sólida dos pilares de arquitetura, segurança,
+                billing, suporte e do modelo de responsabilidade compartilhada,
+                além de entender como a AWS entrega soluções escaláveis e
+                confiáveis para diferentes tipos de aplicações. Essa
+                certificação reforça minha base como profissional de cloud e me
+                prepara para níveis mais avançados dentro do ecossistema AWS.
+              </p>
+            </div>
+
             <div className="text-baseline space-y-3">
               <h3 className="text-sm font-semibold md:text-lg">
                 English - ESL (2025)
@@ -68,20 +108,6 @@ const Home = () => {
                 tecnologia e negócios, onde o inglês é frequentemente o idioma
                 principal de documentação, ferramentas e comunicação
                 internacional.
-              </p>
-            </div>
-
-            <div className="text-baseline space-y-3">
-              <h3 className="text-sm font-semibold md:text-lg">
-                Fundamentos do ReactJS - Rocketseat (2025)
-              </h3>
-              <p className="text-sm text-gray-600 dark:text-gray-400 md:text-base">
-                Curso aprofundado ao React, onde aprimorei meus conhecimentos
-                sobre componentes, props, estado (state), hooks e o ciclo de
-                vida de aplicações em React. Aprendi na prática como estruturar
-                interfaces modernas, reutilizáveis e performáticas. Essa base
-                fortalece meu desenvolvimento front-end com foco em boas
-                práticas e escalabilidade.
               </p>
             </div>
 
